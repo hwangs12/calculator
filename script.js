77d8cfcc9c5098cd809113249b3bd6ca02262a12
Jun Hwang <37169222+hwangs12@users.noreply.github.com>
2021-10-23 23:27:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -26,152 +26,215 @@ INSTRUCTIONS:
 
 // Once you have read the above messages, you can delete all comments. 
 import * as React from "https://cdn.skypack.dev/react@17.0.1";
-import { useState, useEffect } from "https://cdn.skypack.dev/react@17.0.1";
+import {useState, useEffect} from "https://cdn.skypack.dev/react@17.0.1";
 import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
 
 const buttons = [
-{ name: "clear",
-  symbol: "C" },
-{ name: "square-root",
-  symbol: String.fromCharCode(8730) },
-{ name: "plus-minus",
-  symbol: String.fromCharCode(177) },
-{ name: "percent",
-  symbol: String.fromCharCode(37) },
-{ name: "seven",
-  symbol: "7" },
-{ name: "eight",
-  symbol: "8" },
-{ name: "nine",
-  symbol: "9" },
-{ name: "divide",
-  symbol: String.fromCharCode(247) },
-{ name: "four",
-  symbol: "4" },
-{ name: "five",
-  symbol: "5" },
-{ name: "six",
-  symbol: "6" },
-{ name: "multiply",
-  symbol: String.fromCharCode(215) },
-{ name: "one",
-  symbol: "1" },
-{ name: "two",
-  symbol: "2" },
-{ name: "three",
-  symbol: "3" },
-{ name: "subtract",
-  symbol: String.fromCharCode(8722) },
-{ name: "zero",
-  symbol: "0" },
-{ name: "decimal",
-  symbol: "." },
-{ name: "equals",
-  symbol: String.fromCharCode(61) },
-{ name: "add",
-  symbol: String.fromCharCode(43) }];
+  {name: "clear",
+  symbol: "C", 
+  type: "unary"},
+  {name: "square-root",
+  symbol: String.fromCharCode(8730), 
+  type: "binary"},
+  {name: "plus-minus",
+  symbol: String.fromCharCode(177),
+  type: "binary"},
+  {name: "percent",
+  symbol: String.fromCharCode(37), 
+  type: "binary"},
+  {name: "seven",
+  symbol: "7", 
+  type: "unary"},
+  {name: "eight",
+  symbol: "8",
+  type: "unary"},
+  {name: "nine",
+  symbol: "9", 
+  type: "unary"},
+  {name: "divide",
+  symbol: String.fromCharCode(247), 
+  type: "ternary"},
+  {name: "four",
+  symbol: "4",
+  type: "unary"},
+  {name: "five",
+  symbol: "5",
+  type: "unary"},
+  {name: "six",
+  symbol: "6",
+  type: "unary"},
+  {name: "multiply",
+  symbol: String.fromCharCode(215),
+  type: "ternary"},
+  {name: "one",
+  symbol: "1",
+  type: "unary"},
+  {name: "two",
+  symbol: "2", 
+  type: "unary"},
+  {name: "three",
+  symbol: "3",
+  type: "unary"},
+  {name: "subtract",
+  symbol: String.fromCharCode(8722),
+  type: "ternary"},
+  {name: "zero",
+  symbol: "0", 
+  type: "unary"},
+  {name: "decimal",
+  symbol: ".", 
+  type: "decimal"},
+  {name: "equals",
+  symbol: String.fromCharCode(61), 
+  type: "unary"},
+  {name: "add",
+  symbol: String.fromCharCode(43),
+  type: "ternary"}
+  
+]
 
-
-
-const operations = ["÷", "×", "−", "+"];
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-
-
-
+const operations = ["÷","×","−","+"]
+const numbers = ['0','1','2','3','4','5','6','7','8','9']
+      
+function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
+      
+function cutStartWithZero(str) { return str.length < 1 ? '' : str.slice(1) }
 
 const App = () => {
-  const [text, setText] = useState('0');
+  const [text, setText] = useState({value1: '0', operation: '', value2: ''})
 
-
-
-  const showText = e => {
+  
+  
+  const showText = (e) => {
+    let dataType = e.target.getAttribute('data')
+    let dataText = e.target.textContent
+    console.log('is it entered before error occurs?: ', dataText)
     setText(oldState => {
-      let newState = oldState + e.target.textContent;
-      if (newState.includes('..')) {
-        var index = newState.indexOf('..');
-        newState = newState.slice(0, index) + newState.slice(index + 1);
-      }
-
-      if (newState[0] === '0' && numbers.includes(newState[1])) {
-        newState = newState.slice(1);
-      }
-
-      if (operations.includes(newState[newState.length - 3]) && newState[newState.length - 2] === '0' && numbers.includes(newState[newState.length - 1])) {
-        newState = newState.slice(0, newState.length - 2) + newState.slice(newState.length - 1);
-      }
-
-      if (newState.indexOf('.') !== -1 && newState.indexOf('.') !== newState.length - 1 && newState[newState.length - 1] === ".") {
-        newState = newState.slice(0);
-      }
-
-      console.log(newState.match(/[÷×+\−]|\d+(\.\d+)?/g));
-
-
-      return newState;
-    });
-  };
-
-  const compute = () => {
-    let operationArray = text.match(/[÷×+\−]|\d+(\.\d+)?/g);
-    console.log(operationArray);
-    let targetOperatorIndex;
-    while (operationArray.length > 1) {
-      if (operationArray.indexOf("÷") !== -1) {
-        targetOperatorIndex = operationArray.indexOf("÷");
-        let computedValue = Number(operationArray[targetOperatorIndex - 1]) / Number(operationArray[targetOperatorIndex + 1]);
-        operationArray.splice(targetOperatorIndex - 1, 3, computedValue);
-      } else if (operationArray.indexOf("×") !== -1) {
-        targetOperatorIndex = operationArray.indexOf("×");
-        let computedValue = Number(operationArray[targetOperatorIndex - 1]) * Number(operationArray[targetOperatorIndex + 1]);
-        operationArray.splice(targetOperatorIndex - 1, 3, computedValue);
-      } else if (operationArray.indexOf("−") !== -1) {
-        targetOperatorIndex = operationArray.indexOf("−");
-        let computedValue = Number(operationArray[targetOperatorIndex - 1]) - Number(operationArray[targetOperatorIndex + 1]);
-        operationArray.splice(targetOperatorIndex - 1, 3, computedValue);
+      console.log(oldState)
+      if (oldState.operation === '') {
+        if (oldState.value1 === '0' && dataType !== 'decimal') {
+          return {...oldState, value1: dataText}
+        } else if (oldState.value1.includes('.') && dataType === 'decimal') {
+          return {...oldState}
+        } else {
+          return {...oldState, value1: oldState.value1 + dataText}
+        }
+        return {...oldState}
       } else {
-        targetOperatorIndex = operationArray.indexOf("+");
-        let computedValue = Number(operationArray[targetOperatorIndex - 1]) + Number(operationArray[targetOperatorIndex + 1]);
-        operationArray.splice(targetOperatorIndex - 1, 3, computedValue);
+        if (oldState.value2 === '0' && dataType !== 'decimal') {
+          return {...oldState, value2: dataText}
+        } else if (oldState.value2.includes('.') && dataType === 'decimal') {
+          return {...oldState}
+        } else {
+          return {...oldState, value2: oldState.value2 + dataText}
+        }
+        return {...oldState}
       }
-    }
+    })
+  }
+  
+  const addOperation = (e) => {
+    let dataText = e.target.textContent
+    setText(oldState => {
+      if (oldState.operation !== '' && oldState.value2 !== '' && oldState.value1 !== '') {
+        if (oldState.operation === '+') {
+          return {...oldState, value1: Number(oldState.value1) + Number(oldState.value2), operation: dataText, value2: ''}
+        } else if (oldState.operation === '−') {
+          return {...oldState, value1: Number(oldState.value1) - Number(oldState.value2), operation: dataText, value2: ''}
+        } else if (oldState.operation === '×') {
+          return {...oldState, value1: Number(oldState.value1) * Number(oldState.value2), operation: dataText, value2: ''}
+        } else if (oldState.operation === '÷') {
+          return {...oldState, value1: Number(oldState.value1) / Number(oldState.value2), operation: dataText, value2: ''}
+        }
+      }
+      
+      return {...oldState, operation: dataText}
+    })
+  }
+  
+  const toggleNegative = (e) => {
+    setText(oldState => {
+      if (oldState.operation === '') {
+        if (oldState.value1[0] === '-') {
+          return {...oldState, value1: oldState.value1.slice(1)}
+        } else {
+          
+      return {...oldState, value1: '-' + oldState.value1}
+        }
+      } else {
+        if (oldState.value2[0] === '-') {
+          return {...oldState, value2: oldState.value2.slice(1)}
+        } else {
+          
+        return {...oldState, value2: '-' + oldState.value2}
+        }
+      }
+    })
+  }
+  
+  const compute = () => {
+    setText(oldState => {
+      console.log(oldState)
+      if (oldState.operation !== '' && oldState.value2 !== '' && oldState.value1 !== '') {
+        if (oldState.operation === '+') {
+          return {...oldState, value1: Number(oldState.value1) + Number(oldState.value2), operation: '', value2: ''}
+        } else if (oldState.operation === '−') {
+          return {...oldState, value1: Number(oldState.value1) - Number(oldState.value2), operation: '', value2: ''}
+        } else if (oldState.operation === '×') {
+          return {...oldState, value1: Number(oldState.value1) * Number(oldState.value2), operation: '', value2: ''}
+        } else if (oldState.operation === '÷') {
+          return {...oldState, value1: Number(oldState.value1) / Number(oldState.value2), operation: '', value2: ''}
+        }
+      } else {
+        
+      return {...oldState}
+      }
+      
+    })
+  }
+    
+  const removeText = (e) => {
+    setText({value1: '0', operation: '', value2: ''})
+  }
+  
+  return <main className="calculator">
+    <Screen text={text} />
+    <Buttons showText={showText} removeText={removeText} compute={compute} addOperation={addOperation} toggleNegative={toggleNegative} />
+  </main>
+}
 
-    setText(operationArray[0]);
-  };
+const Screen = ({text}) => {
+  return <div className="screen" id="display">
+    <span>{text.value1} {text.operation} {text.value2}</span>
+  </div>
+}
 
-  const removeText = e => {
-    setText('0');
-  };
-
-  return /*#__PURE__*/React.createElement("main", { className: "calculator" }, /*#__PURE__*/
-  React.createElement(Screen, { text: text }), /*#__PURE__*/
-  React.createElement(Buttons, { showText: showText, removeText: removeText, compute: compute }));
-
-};
-
-const Screen = ({ display, text }) => {
-  return /*#__PURE__*/React.createElement("div", { className: "screen", id: "display" },
-  text);
-
-};
-
-const Buttons = ({ showText, removeText, compute }) => {
-  return /*#__PURE__*/React.createElement("div", { className: "buttons-container" },
-  buttons.map((button, index) => {
-    const { name, symbol } = button;
-    if (name === 'clear') {
-      return /*#__PURE__*/React.createElement("div", { key: index, className: "calc-btn", id: name, onClick: removeText }, /*#__PURE__*/React.createElement(React.Fragment, null, symbol));
-    }
-
-    if (name === 'equals') {
-      return /*#__PURE__*/React.createElement("div", { key: index, className: "calc-btn", id: name, onClick: compute }, /*#__PURE__*/React.createElement(React.Fragment, null, symbol));
-    }
-
-    return /*#__PURE__*/React.createElement("div", { key: index, className: "calc-btn", id: name, onClick: showText }, /*#__PURE__*/React.createElement(React.Fragment, null, symbol));
-  }));
-
-};
+const Buttons = ({showText, removeText, addOperation, compute, toggleNegative}) => {
+  return <div className="buttons-container">
+    {buttons.map((button, index) => {
+      const {name, symbol, type} = button
+      if (name === 'clear') {
+        return <div key={index} className="calc-btn" id={name} data={type} onClick={removeText}><>{symbol}</></div>
+      }
+      
+      if (name === 'equals') {
+        return <div key={index} className="calc-btn" id={name} data={type} onClick={compute}><>{symbol}</></div>
+      }
+      
+      if (name === 'plus-minus') {
+        return <div key={index} className="calc-btn" id={name} data={type} onClick={toggleNegative}><>{symbol}</></div>
+      }
+      
+      if (type === 'ternary') {
+        return <div key={index} className="calc-btn" id={name} data={type} onClick={(e) => addOperation(e)}><>{symbol}</></div>
+      }
+      
+      
+      return <div key={index} className="calc-btn" id={name} data={type} onClick={showText}><>{symbol}</></div>
+    })}
+  </div>
+}
 
 
-
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
+      
+ReactDOM.render(<App />, document.getElementById('root'))
